aeac2d51263f5c439caf4fbf6373bcc1675896fa
Manuel Arango <juan12292004@outlook.com>
2025-06-07 23:11:25 -0500

    Add HabitCheckmark entity class

---
+++ src/habitHandle/domain/entities/HabitCheckmark.ts
@@ -0,0 +1,58 @@
+class HabitCheckmark {
+  private id: number;
+  private habitId: number;
+  private date: Date;
+  private completed: boolean;
+  private createdAt: Date;
+
+  constructor(
+    id: number,
+    habitId: number,
+    date: Date,
+    completed: boolean,
+    createdAt: Date,
+  ) {
+    this.id = id;
+    this.habitId = habitId;
+    this.date = date;
+    this.completed = completed;
+    this.createdAt = createdAt;
+  }
+
+  getId(): number {
+    return this.id;
+  }
+  getHabitId(): number {
+    return this.habitId;
+  }
+  getDate(): Date {
+    return this.date;
+  }
+  isCompleted(): boolean {
+    return this.completed;
+  }
+  getCreatedAt(): Date {
+    return this.createdAt;
+  }
+  setCompleted(completed: boolean): void {
+    this.completed = completed;
+  }
+  setDate(date: Date): void {
+    this.date = date;
+  }
+  setHabitId(habitId: number): void {
+    this.habitId = habitId;
+  }
+  setCreatedAt(createdAt: Date): void {
+    this.createdAt = createdAt;
+  }
+  toJSON(): object {
+    return {
+      id: this.id,
+      habitId: this.habitId,
+      date: this.date,
+      completed: this.completed,
+      createdAt: this.createdAt,
+    };
+  }
+}
